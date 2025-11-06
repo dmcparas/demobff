d8a90a6ee7608f100cc410ca25bde28c12ff4ce6
divyaprakash-tf <divya.prakash@candescent.com>
2025-11-06 12:08:31 +0530

    config changes

--- src/main/java/com/example/bff/model/Constant.java
+++ src/main/java/com/example/bff/model/Constant.java
@@ -4,7 +4,7 @@ public class Constant {
 //    public static final String URL1 = "https://democore.fly.dev";
 //    public static final String URL2 = "https://democore1.fly.dev";
 //    public static final String URL3 = "https://democore2.fly.dev";
-    public static final String URL1 = "https://democore.internal";
-    public static final String URL2 = "https://democore1.internal";
-    public static final String URL3 = "https://democore2.internal";
+    public static final String URL1 = "https://democore.internal:8080";
+    public static final String URL2 = "https://democore1.internal:8080";
+    public static final String URL3 = "https://democore2.internal:8080";
 }
